049f13be9ea6a2d388ff396fe350a44e1bd711d0
sakshi jadhav <sakshisjadhav.120@gmail.com>
2025-04-10 19:13:22 +0530

    updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://butterflyweb.onrender.com',
+  origin: 'http://localhost:5000',
   credentials: true,
 }));
 app.use(express.json());
