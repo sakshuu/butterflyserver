2bd7c67fc0d839900393a3f44689ef98596a1377
sakshi jadhav <sakshisjadhav.120@gmail.com>
2025-04-10 18:44:50 +0530

    updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://butterfly-web-lyart.vercel.app',
+  origin: 'https://butterflyweb.onrender.com',
   credentials: true,
 }));
 app.use(express.json());
