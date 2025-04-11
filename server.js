5297764883d6201727ddb4ada948c53c84319967
sakshi jadhav <sakshisjadhav.120@gmail.com>
2025-04-11 11:39:55 +0530

    updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:5000',
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json());
